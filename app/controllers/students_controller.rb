2d257de1971a9b9d3996f2f90e343ed096ca7729
Cesar <cesarmota@ymail.com>
2020-02-03 10:53:52 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,12 +5,30 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  #renders properly
+  #renders the first name in a h1 tag
+  #renders the last name in a h1 tag
+  #renders the active status if the user is inactive
+  #renders the active status if the user is active
   def show
+    @student = Student.find(params[:id])
+  end
+   
+
+  #Should mark an inactive student as active 
+  #Should mark an active student as inactive 
+  #Should redirect to the student show page 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
 
     def set_student
       @student = Student.find(params[:id])
+   
     end
 end
