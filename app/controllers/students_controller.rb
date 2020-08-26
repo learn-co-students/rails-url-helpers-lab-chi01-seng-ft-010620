ce80bfa66bb4132593099bdb2424aa26adb2c0ed
lindsayMecher <lindsaymecher@gmail.com>
2020-08-26 10:38:38 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,50 +1,16 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-
   def index
-    @students = Student.all
-
+    @students = Student.all 
   end
-
+  
   def show
     @student = Student.find(params[:id])
-  end
+  end 
 
-  def new
-
-  end
-
-  def edit
-  end
-
-  def create
-    @student = Student.new(params[:id])
-    redirect_to "/students"
-  end
-
-  def destroy
-    @student.find_by(params[:id])
-    @student.destroy
-  end
-
-  def activate
+  def activate 
     @student = Student.find(params[:id])
-    if @student.active == false
-      @student.active = true
-    else
-      @student.active = false
-    end
-    @student.save
-    redirect_to student_path
-  end
+    @student.update(active: !@student.active)
+    redirect_to student_path(@student)
+  end 
 
-  private
-
-    def set_student
-      @student = Student.find_by(params[:id])
-    end
-
-    def student_params
-      params.require(:student).permit(:first_name, :last_name)
-    end
 end
